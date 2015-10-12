14db11dac7dfd3e232266bd27511938a59076722
mansrz <mansrz@gmail.com>
2015-10-12 16:30:00 -0500

    Up project

---
+++ web/static/js/main.js
@@ -0,0 +1,101 @@
+$(function() {
+
+  "use strict";
+
+  var window_width = $(window).width();
+
+  /*Preloader*/
+  $(window).load(function() {
+    setTimeout(function() {
+      $('body').addClass('loaded');
+    }, 200);
+  });
+
+
+  // Search class for focus
+  $('.header-search-input').focus(
+  function(){
+      $(this).parent('div').addClass('header-search-wrapper-focus');
+  }).blur(
+  function(){
+      $(this).parent('div').removeClass('header-search-wrapper-focus');
+  });
+  // Check first if any of the task is checked
+  $('#task-card input:checkbox').each(function() {
+    checkbox_check(this);
+  });
+
+  // Task check box
+  $('#task-card input:checkbox').change(function() {
+    checkbox_check(this);
+  });
+
+  // Check Uncheck function
+  function checkbox_check(el){
+      if (!$(el).is(':checked')) {
+          $(el).next().css('text-decoration', 'none'); // or addClass
+      } else {
+          $(el).next().css('text-decoration', 'line-through'); //or addClass
+      }
+  }
+
+  /*----------------------
+  * Plugin initialization
+  ------------------------*/
+
+  // Materialize Slider
+  $('.slider').slider({
+    full_width: true
+  });
+
+  // Materialize Dropdown
+  $('.dropdown-button').dropdown({
+    inDuration: 300,
+    outDuration: 125,
+    constrain_width: true, // Does not change width of dropdown to that of the activator
+    hover: false, // Activate on click
+    alignment: 'left', // Aligns dropdown to left or right edge (works with constrain_width)
+    gutter: 0, // Spacing from edge
+    belowOrigin: true // Displays dropdown below the button
+  });
+
+  // Materialize Tabs
+  $('.tab-demo').show().tabs();
+  $('.tab-demo-active').show().tabs();
+
+  // Materialize Parallax
+  $('.parallax').parallax();
+  $('.modal-trigger').leanModal();
+
+  // Materialize scrollSpy
+  $('.scrollspy').scrollSpy();
+
+  // Materialize tooltip
+  $('.tooltipped').tooltip({
+    delay: 50
+  });
+
+  // Materialize sideNav
+
+  //Main Left Sidebar Menu
+  $('.sidebar-collapse').sideNav({
+    edge: 'left', // Choose the horizontal origin
+  });
+
+  // FULL SCREEN MENU (Layout 02)
+  $('.menu-sidebar-collapse').sideNav({
+        menuWidth: 240,
+        edge: 'left', // Choose the horizontal origin
+        //defaultOpen:true // Set if default menu open is true
+      });
+
+  // HORIZONTAL MENU (Layout 03)
+  $('.dropdown-menu').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true // Displays dropdown below the button
+    });
+});
